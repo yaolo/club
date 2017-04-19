1b82825dbf154db8bbe2806e3c9253511656332a
liuyao <liuyao@liuyaodeMacBook-Pro.local>
2017-04-19 16:11:58 +0800

    添加个人中心页面

--- js/common.js
+++ js/common.js
@@ -6,8 +6,25 @@
                 main.pbstyle();
                 $('.login').addClass('show');
             });
-            $('#register,.close').click(function () {
+            $('#btn,.close').click(function () {
                 main.pbremove();
+            });
+            $('#upload').change(function () {
+                //判断浏览器是否支持FileReader接口
+                if (typeof FileReader == 'undefined') {
+                    $('.tips').html('当前浏览器不支持FileReader接口，请更换为最新版本的浏览器!');
+                    //使选择控件不可操作
+                    $('.tochange').attr("disabled", "disabled");
+                }
+                var file = $('#upload')[0].files[0];
+                console.log(file);
+                var reader = new FileReader();
+                console.log(reader);
+                reader.onload = function (e) {
+                    var img = document.getElementById("uploadimg");
+                    img.src = e.target.result;
+                }
+                reader.readAsDataURL(file);
             });
         },
         pbstyle:function () {
